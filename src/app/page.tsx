ebd661d07a7ccef9379ef58c3569ce2fd432a073
Vijay <vijaykumar4495@gmail.com>
2023-06-07 01:02:24 +0100

    Complete the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,30 +33,39 @@ export default function Home() {
                 Era of Job Search and Say Farewell to Traditional Methods
               </p>
               {/* gradient button */}
-              <button
-                className="
-              bg-gradient-to-r from-[#5350FF]   to-[#DF0F99] text-white px-10 py-3 rounded-full my-10 shadow-md hover:shadow-lg
+              <div className="flex">
+                <button
+                  className="
+              bg-gradient-to-r from-[#5350FF]   to-[#DF0F99] text-white px-10  rounded-full my-12 shadow-md hover:shadow-lg
+              w-1/2
+              py-3
+              text-sm
               "
-              >
-                Sign Up
-              </button>
-              <button
-                className="
-            border px-10 py-3 rounded-full my-10 ml-3 inline-block hover:shadow-lg bg-white"
-              >
-                Watch Demo
-              </button>
+                >
+                  Sign Up
+                </button>
+                <button
+                  className="
+            border px-10  py-3 rounded-full my-12  ml-3 inline-block hover:shadow-lg bg-white
+            w-1/2 text-sm
+            "
+                >
+                  Watch Demo
+                </button>
+              </div>
 
               <img
                 src="images/arrow-objects.svg"
-                className="absolute bottom-0 left-0 w-52 h-52 -z-20 opacity-40 hidden md:inline-block"
+                className="absolute bottom-0  w-52 h-52 -z-20 opacity-40 hidden md:inline-block
+                -left-20
+                "
               />
             </div>
             <div className="hidden md:w-3/5  md:block	">
-              <img src="/images/main-illustration.svg" className="relative -top-28 h-[640px]" />
+              <img src="/images/main-illustration.png" className="relative -top-28 h-[640px]" />
             </div>
           </div>
-          
+
           <img
             src="/images/trust-brand.png"
             className="
@@ -74,18 +83,23 @@ export default function Home() {
           w-[500px] h-[500px] opacity-30 bg-[#FACD49] rounded-full blur-3xl absolute  -right-[27rem] -bottom-96"
           ></div>
 
-          <img src="/images/main-illustration.svg" className="block md:hidden relative" />
+          <img src="/images/main-illustration.png" className="block md:hidden relative" />
         </section>
       </div>
 
       <section className="relative">
-        <img src="/images/rocket.png" className="absolute -bottom-24 -right-6 h-56" />
+        <img
+          src="/images/rocket.png"
+          className="absolute -bottom-44 md:-bottom-24 -right-6 h-40 md:h-56
+            hidden md:block
+          "
+        />
         <div className="flex flex-col md:flex-row">
           <div className="w-full md:w-1/2">
-            <img src="/images/effortless-connections.svg" />
+            <img src="/images/effortless-connections.png" className="hidden md:block" />
           </div>
           <div className="w-full md:w-1/2">
-            <div className="w-2/3 mx-auto pt-24">
+            <div className="w-[90%] md:w-2/3 mx-auto pt-3 md:pt-24">
               <p className="py-6">&nbsp;</p>
               <h1 className="text-4xl md:text-6xl my-6 font-bold">
                 Effortless&nbsp;
@@ -102,39 +116,17 @@ export default function Home() {
           </div>
         </div>
       </section>
-
+      <div className="hidden md:block">
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+      </div>
       <section className="relative">
-        <img src="/images/rocket.png" className="absolute -bottom-24 -right-6 h-56" />
-        <div className="flex">
-          <div className="w-1/2">
-            <img src="/images/effortless-connections.svg" />
-          </div>
-          <div className="w-1/2">
-            <div className="w-2/3 mx-auto pt-24">
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-1/2">
+            <div className="w-[90%] md:w-2/3 mx-auto pt-3 md:pt-24">
               <p className="py-6">&nbsp;</p>
-              <h1 className="text-6xl my-6 font-bold">
-                Effortless&nbsp; <br />
-                <span className="text-primary">Connections</span>
-              </h1>
-              <p className="text-slate-500 leading-9 text-lg">
-                For job givers, we provide a seamless and efficient hiring process. Our smart
-                candidate matching algorithms help you find the perfect fit for your organization.
-                Save time and resources by connecting directly with high-quality candidates referred
-                by trusted professionals.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <section className="relative">
-        <div className="flex">
-          <div className="w-1/2">
-            <div className="w-2/3 mx-auto pt-24">
-              <p className="py-6">&nbsp;</p>
-              <h1 className="text-6xl my-6 font-bold">
-                Tap into the &nbsp;
+              <h1 className="text-4xl md:text-6xl my-6 font-bold">
+                Tap into the&nbsp;
                 <span className="text-primary block">Power of Referrals</span>
               </h1>
               <p className="text-slate-500 leading-9 text-lg">
@@ -145,21 +137,28 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="w-1/2 text-right relative ">
-            <img src="/images/power-of-referrals.svg" className="h-[600px] relative -right-9" />
+          <div className="w-full md:w-1/2 text-right relative">
+            <img
+              src="/images/power-of-referrals.png"
+              className="h-[600px] hidden md:block relative -right-20"
+            />
           </div>
         </div>
       </section>
+
       <section className="relative">
-        <img src="/images/resume.png" className="absolute top-20 right-12" />
-        <div className="flex">
-          <div className="w-1/2">
-            <img src="/images/hiring-process.svg" />
+        <img
+          src="/images/resume.png"
+          className="absolute w-16 md:w-auto top-10 md:top-20 right-12 hidden md:block"
+        />
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-1/2">
+            <img src="/images/hiring-process.png" className="md:block hidden" />
           </div>
-          <div className="w-1/2">
-            <div className="w-2/3 mx-auto pt-24">
-              <p className="py-6">&nbsp;</p>
-              <h1 className="text-6xl my-6 font-bold">
+          <div className="w-full md:w-1/2">
+            <div className="w-[90%] md:w-2/3 mx-auto  pt-3 md:pt-24">
+              <p className="py-6 md:py-6">&nbsp;</p>
+              <h1 className="text-4xl md:text-6xl my-6 font-bold">
                 Streamlined &nbsp;
                 <span className="text-primary block">Hiring Process</span>
               </h1>
@@ -173,34 +172,27 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       {/* clearfix */}
-      <div className="clearfix py-10 my-10"></div>
+      <div className="clearfix py-12 my-1 md:py-10 md:my-10 "></div>
 
       <section>
-        <div className="flex">
-          <div className="w-1/2">
-            <div className="w-2/3 mx-auto">
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full md:w-1/2">
+            <div className="w-[90%] md:w-2/3  mx-auto">
               <h3 className="text-xl text-primary my-3">KEY FEATURES</h3>
-              <h1 className="text-6xl my-4 font-bold">Features That Empower You</h1>
+              <h1 className="text-4xl md:text-6xl my-4 font-bold">Features That Empower You</h1>
               <p className="text-slate-500 my-7 text-lg">
                 Maximize Your Potential with Powerful Platform Features
               </p>
               <div className="p-4 bg-white z-10 rounded-lg mt-3 border hover:shadow-sm cursor-pointer">
                 <div className="flex flex-row">
                   <div className="flex flex-col">
-                    <span
-                      className="
-                        bg-[#FF5722]
-                        w-16
-                        h-16
-                        p-3
-                        rounded-2xl
-                      "
-                    >
+                    <span className="bg-[#FF5722] w-16 h-16 p-3 rounded-2xl">
                       <img src="/images/user.svg" />
                     </span>
                   </div>
-                  <div className="flex flex-col  justify-center pl-6 font-semibold">
+                  <div className="flex flex-col justify-center pl-6 font-semibold">
                     Personalized Job Recommendations
                   </div>
                 </div>
@@ -209,15 +201,7 @@ export default function Home() {
               <div className="p-4 bg-white rounded-lg mt-3 border hover:shadow-sm cursor-pointer">
                 <div className="flex flex-row">
                   <div className="flex flex-col">
-                    <span
-                      className="
-                        bg-[#7D65D5]
-                        w-16
-                        h-16
-                        p-3
-                        rounded-2xl
-                      "
-                    >
+                    <span className="bg-[#7D65D5] w-16 h-16 p-3 rounded-2xl">
                       <img src="/images/match.svg" />
                     </span>
                   </div>
@@ -229,15 +213,7 @@ export default function Home() {
               <div className="p-4 bg-white rounded-lg mt-3 border hover:shadow-sm cursor-pointer">
                 <div className="flex flex-row">
                   <div className="flex flex-col">
-                    <span
-                      className="
-                        bg-[#FACD49]
-                        w-16
-                        h-16
-                        p-3
-                        rounded-2xl pt-5
-                      "
-                    >
+                    <span className="bg-[#FACD49] w-16 h-16 p-3 rounded-2xl pt-5">
                       <img src="/images/ats.svg" className="justify-center flex flex-col" />
                     </span>
                   </div>
@@ -249,39 +225,29 @@ export default function Home() {
               <div className="p-4 bg-white rounded-lg mt-3 border hover:shadow-sm cursor-pointer">
                 <div className="flex flex-row">
                   <div className="flex flex-col">
-                    <span
-                      className="
-                        bg-[#F85E9F]
-                        w-16
-                        h-16
-                        p-3
-                        rounded-2xl 
-                      "
-                    >
+                    <span className="bg-[#F85E9F] w-16 h-16 p-3 rounded-2xl">
                       <img src="/images/pie.svg" />
                     </span>
                   </div>
-                  <div
-                    className="flex flex-col
-                    justify-center pl-6 font-semibold
-                  "
-                  >
+                  <div className="flex flex-col justify-center pl-6 font-semibold">
                     Data-Driven Insights
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="w-1/2 relative">
+          <div className="w-full md:w-1/2 relative hidden md:block">
             <img
               src="images/you-got-new-job.png"
-              className="relative -right-44  -top-20 h-[700px]"
+              className="relative -right-44 -top-20 h-[700px]"
             />
           </div>
         </div>
       </section>
+
       <p>&nbsp;</p>
       <p>&nbsp;</p>
+
       <section className="relative h-[500px]">
         <div className="container mx-auto">
           {/* testimonial slider */}
@@ -289,7 +255,7 @@ export default function Home() {
             <h1 className="text-center text-primary mt-5 text-2xl font-bold">Testimonials</h1>
             <p className="text-3xl text-center mt-5">Trust our clients</p>
             <div className="flex flex-row">
-              <div className="w-[10%]">
+              <div className="hidden md:block md:w-[10%]">
                 <button
                   className="bg-white text-black border p-2 rounded-full
                   w-16 h-16 mt-20 hover:shadow-lg 
@@ -310,7 +276,7 @@ export default function Home() {
                   </svg>
                 </button>
               </div>
-              <div className="w-[80%]">
+              <div className="w-[100%] md:w-[80%]">
                 <div>
                   <div className="text-center">
                     <img
@@ -425,7 +391,7 @@ export default function Home() {
                   <button className="bg-gray-300 ml-3 w-3 cursor-pointer h-3 rounded-full"></button>
                 </p>
               </div>
-              <div className="w-[10%]">
+              <div className="hidden md:block md:w-[10%]">
                 <button
                   className="bg-primary text-white border p-2 rounded-full
                   w-16 h-16 mt-20 hover:shadow-lg
@@ -451,14 +417,23 @@ export default function Home() {
         </div>
         <img
           src="images/testimonial-blob.svg"
-          className="absolute w-screen -top-[450px] left-0 pointer-events-none -z-10"
+          className="
+            hidden md:block
+          absolute w-screen -top-[450px] left-0 pointer-events-none -z-10"
         />
       </section>
-      <div className="container mx-auto">
-        <section className="relative mt-20">
-          <div className="p-20 py-20 relative bg-[#FFFBF0] w-auto mx-auto mt-10 text-center">
-            <img src="/images/graphic-elements.svg" className="absolute -top-10 -left-10" />
-            <img src="/images/rings.svg" className="absolute -right-28 -bottom-40" />
+
+      <div className="container mx-auto ">
+        <section className="relative mt-20 ">
+          <div className="p-4 md:p-20 md:py-20 relative bg-[#FFFBF0] w-auto mx-auto mt-10 text-center">
+            <img
+              src="/images/graphic-elements.svg"
+              className="hidden md:block absolute -top-10 -left-10"
+            />
+            <img
+              src="/images/rings.svg"
+              className="hidden md:block absolute -right-28  -bottom-40"
+            />
             <h3 className="text-primary text-lg font-semibold uppercase">Be the First to Know</h3>
             <p>&nbsp;</p>
             <p className="text-4xl font-medium my-6">
@@ -471,26 +446,27 @@ export default function Home() {
             </p>
             <p>&nbsp;</p>
             <div className="flex justify-center mt-5">
-              <div className="flex flex-row">
+              <div className="flex flex-col md:flex-row">
                 <div className="flex flex-col">
                   <input
                     type="text"
                     placeholder="Enter your email"
                     className="p-4 border border-gray-300 rounded-lg mr-3
-                    w-96
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-[#5D50C6]
-                    focus:border-transparent
-
-                    "
+        w-full md:w-96
+        focus:outline-none
+        focus:ring-2
+        focus:ring-[#5D50C6]
+        focus:border-transparent"
                   />
                 </div>
                 <div className="flex flex-col">
-                  <button className="bg-[#5D50C6] text-white p-4 rounded-2xl">Subscribe</button>
+                  <button className="bg-[#5D50C6] text-white p-4 rounded-2xl mt-3 md:mt-0">
+                    Subscribe
+                  </button>
                 </div>
               </div>
             </div>
+
             <p>&nbsp;</p>
             <p>&nbsp;</p>
           </div>
