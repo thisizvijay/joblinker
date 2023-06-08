6fa4dce7859e9fc3514ee8c6143fa4ad5b9103ef
Vijay <vijaykumar4495@gmail.com>
2023-06-08 05:28:12 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
   const [blockID, setblockID] = useState("");
   const [emailID, setemailID] = useState("");
   const [sourceX, setsourceX] = useState("");
+  const [videoModel, setvideoModel] = useState(false);
 
   const constructUrl = () => {
     let url = "https://sk2ck7ohu9y.typeform.com/to/ob8UZl9V";
@@ -93,7 +94,7 @@ export default function Home() {
                   <div className="absolute inset-0">
                     <div className="h-full w-[90%] mx-auto bg-white opacity-100">
                       <iframe
-                      id="myIframe"
+                        id="myIframe"
                         src={"https://eu-submit.jotform.com/231576852462361"}
                         frameBorder="0"
                         className="w-full h-full"
@@ -102,6 +103,67 @@ export default function Home() {
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        )}
+        {videoModel && (
+          <div>
+            {/* overlay */}
+            <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+
+            <div
+              id="defaultModal"
+              tabIndex={-1}
+              className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex"
+              aria-modal="true"
+              role="dialog"
+            >
+              <div className="relative w-full max-w-2xl max-h-full">
+                {/* Modal content */}
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                  {/* Modal header */}
+                  <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                    <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                      Introducing JobLinkr.io
+                    </h3>
+                    <button
+                      type="button"
+                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-hide="defaultModal"
+                      onClick={() => setvideoModel(false)}
+                    >
+                      <svg
+                        aria-hidden="true"
+                        className="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <span className="sr-only">Close modal</span>
+                    </button>
+                  </div>
+                  {/* Modal body */}
+                  <div className="p-6 space-y-6">
+                    {/* video */}
+                    <video controls className="w-full"
+                      autoPlay
+                    >
+                      <source src="/introduction-video.mp4"
+                        
+                      type="video/mp4" />
+                      Your browser does not support the video tag.
+                      </video>
+                  </div>
+                  {/* Modal footer */}
+                </div>
+              </div>
+            </div>
             </div>
           </div>
         )}
@@ -147,6 +209,7 @@ export default function Home() {
             border px-10  py-3 rounded-full my-12  ml-3 inline-block hover:shadow-lg bg-white
             w-1/2 text-sm
             "
+                  onClick={() => setvideoModel(true)}
                 >
                   Watch Demo
                 </button>
